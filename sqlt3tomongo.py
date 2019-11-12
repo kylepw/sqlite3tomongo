e4f955135700f2975e48f8b0b1bd3e8732ba79e2
Kyle <kylepw@gmail.com>
2019-11-12 20:25:51 +0900

    update sqlt3mongo script

--- sqlt3tomongo.py
+++ sqlt3tomongo.py
@@ -1,16 +1,27 @@
-#!/usr/bin/env python3
-""" sqlite3import.py
-    ~~~~~~~~~~~~~~~~
+""" sqlt3import
+    ~~~~~~~~~~~
 
-    Import SQLite3 database files into mongod instance.
+    Import SQLite3 database into MongoDB database.
+
+    Usage:
+        $ sqlt3import data.db --host 'mongodb://mydatabase:27017'
+
+    Notes:
+        Database named after filename by default (i.e. `data` if filename
+        is `data.db`). Host defaults to 'mongodb://localhost:27017' if none
+        provided. Collections named after SQL tables. Collections are dropped
+        before inserts by default. Use '--append' option to keep collections.
 
 """
+import argparse
 import json
 import logging
 import os
 from pymongo import MongoClient
 import sqlite3
 import sys
+
+__version__ = '0.1.0'
 
 logging.basicConfig(
     format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
@@ -29,10 +40,7 @@ def load_sql(db_path):
         c.execute("SELECT name FROM sqlite_master WHERE type='table' ORDER BY name;")
         tables = c.fetchall()
 
-        data = {
-            'db': os.path.splitext(os.path.basename(db_path))[0],
-            'collections': {}
-        }
+        data = {'db': os.path.splitext(os.path.basename(db_path))[0], 'collections': {}}
         for table in tables:
             c.execute(f"SELECT * FROM {table['name']}")
             rows = c.fetchall()
@@ -53,17 +61,18 @@ def load_sql(db_path):
     finally:
         conn.close()
 
+
 def dump_mongo(data, uri=None):
     """Dump collection data into mongod"""
     uri = uri or 'mongodb://localhost:27017'
     client = MongoClient(uri)
     try:
-        del data['db']
         if 'db' not in data:
             raise ValueError("'db' key missing in dictionary")
         if 'collections' not in data:
             raise ValueError("'collections' key missing in dictionary")
         db = client[data['db']]
+        results = []
         for coll_name, docs in data['collections'].items():
             coll = db[coll_name]
             coll.drop()
@@ -71,35 +80,54 @@ def dump_mongo(data, uri=None):
             coll.insert_many(docs)
             expected_count = len(data['collections'][coll_name])
             count = coll.count_documents({})
-            logger.info('Inserted %s docs into %s.%s on %s', count, data['db'], coll_name, uri)
-            assert (count == expected_count), "Only %s of %s docs imported into '%s' collection" % (count, expected_count, data['db'])
+            result_str = '%s docs inserted into %s.%s' % (count, data['db'], coll_name)
+            logger.info(result_str)
+            assert count == expected_count, (
+                "Only %s of %s docs imported into '%s' collection"
+                % (count, expected_count, data['db'])
+            )
+            print(result_str)
     except Exception:
         print('Failed to import data into mongod. Check the logs.')
         raise
     finally:
         client.close()
 
-def main(argv):
+
+def get_parser():
+    parser = argparse.ArgumentParser(description='Import SQLite3 database into MongoDB')
+    parser.add_argument('dbfile', metavar='file', help='sqlite3 database file')
+    parser.add_argument(
+        '--host',
+        dest='uri',
+        metavar='string',
+        help='mongodb uri string',
+        default='mongodb://localhost:27017',
+    )
+    parser.add_argument('-v', '--version', action='version', version=__version__)
+    return parser
+
+
+def main():
+    parser = get_parser()
+    args = vars(parser.parse_args())
     try:
-        if len(argv) < 2:
-            print(f"usage: {argv[0]} [database file] 'mongod uri'")
-            exit(1)
-        db_path = os.path.abspath(argv[1])
+        db_path = os.path.abspath(args['dbfile'])
         if not os.path.isfile(db_path):
-            err_msg = f"`{argv[1]}` file not found."
+            err_msg = f"Invalid file: {args['dbfile']}"
             print(err_msg)
             raise OSError(err_msg)
-        uri = argv[2] if len(argv) > 2 else None
 
         # load and dump data (sql to mongo)
         data = load_sql(db_path)
-        dump_mongo(data, uri)
-
-        print('Done.')
+        dump_mongo(data, args['uri'])
     except KeyboardInterrupt:
         print('\nBye!')
     except Exception:
-        logger.exception('Something went wrong')
+        logger.exception('Something went wrong.')
+    else:
+        print('Done.')
+
 
 if __name__ == '__main__':
-    main(sys.argv)
+    main()
